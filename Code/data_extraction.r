a82561ae0a0221255a71ea1d040cfc5878ef530a
jacobmcl7 <jacob.mcloughlin@warwick.ac.uk>
2025-10-02 21:54:27 +0100

    introduce a loop in the data extraction, and do some very preliminary geocoding

--- Code/data_extraction.r
+++ Code/data_extraction.r
@@ -1,42 +1,35 @@
 # this extracts the data to create one large dataset of dated crimes that can be geocoded
 
-library(openxlsx)
+library(writexl)
 library(tidyverse)
 
-setwd("~/Economics/Papers (WIP)/Crime and night tubes")
+setwd("~/Economics/Papers (WIP)")
 
-# load in the first month of 2015 data
-crime_data <- as.data.frame(read.csv("Data/2015-01/2015-01-metropolitan-street.csv", stringsAsFactors = FALSE))
+# make a different datafile for each year, so the files aren't too big
 
-# merge with the rest of the data
-for (i in 2:12) {
-    # get the correct month format
-    month <- ifelse(i < 10, paste0("0", i), i)
-    # read in the data
-    temp_data <- as.data.frame(read.csv(paste0("Data/2015-", month, "/2015-", month, "-metropolitan-street.csv"), stringsAsFactors = FALSE))
-    # merge with the main dataset
-    crime_data <- rbind(crime_data, temp_data)
-}  
+for (year in 2015:2017) {
 
-for (j in 2016:2017) {
-    for (i in 1:12) {
+    # load in the first month of the year data
+    crime_data <- as.data.frame(read.csv(paste0("Crime and night tubes/Data/", year, "-01/", year, "-01-metropolitan-street.csv"), stringsAsFactors = FALSE))
+    
+    # merge with the rest of the data
+    for (i in 2:12) {
         # get the correct month format
         month <- ifelse(i < 10, paste0("0", i), i)
-        temp_data <- as.data.frame(read.csv(paste0("Data/", j, "-", month, "/", j, "-", month, "-metropolitan-street.csv"), stringsAsFactors = FALSE))
+        # read in the data
+        temp_data <- as.data.frame(read.csv(paste0("Crime and night tubes/Data/", year, "-", month, "/", year, "-", month, "-metropolitan-street.csv"), stringsAsFactors = FALSE))
+        # merge with the main dataset
         crime_data <- rbind(crime_data, temp_data)
-    }  
+    }
+    
+    # drop irrelevant columns
+    crime_data <- as.data.frame(crime_data) %>%
+        select(-c(Crime.ID, Reported.by, Falls.within, Context))
+    
+    # drop observations with no coordinates
+    crime_data <- crime_data %>%
+        filter(!is.na(Longitude) & !is.na(Latitude))
+    
+    # export this as an excel file to be read into ArcGIS
+    write_xlsx(crime_data, paste0("Crime and night tubes EXTRA DATA/london_crime_data-", year, ".xlsx"))
 }
-
-# drop irrelevant columns
-crime_data <- as.data.frame(crime_data) %>%
-    select(-c(Crime.ID, Reported.by, Falls.within, Context))
-
-# drop observations with no coordinates
-crime_data <- crime_data %>%
-    filter(!is.na(Longitude) & !is.na(Latitude))
-
-# export this as an excel file to be read into ArcGIS
-write.xlsx(crime_data, 'Data/london_crime_data.xlsx')
-
-# for some reason this gives a corrupted file: also export it as csv
-write.csv(crime_data, 'Data/london_crime_data.csv')
--- Code/gis_processing.py
+++ Code/gis_processing.py
@@ -1,2 +1,30 @@
 # this file contains the code for the spatial processing in ArcGIS
 
+# insert the shapefile for stations
+tube_stations = r"C:\Users\jpmcl\OneDrive\Documents\Economics\Papers (WIP)\Crime and night tubes\Data\Underground_Stations\Underground_Stations.shp"
+tube_stations = arcpy.management.MakeFeatureLayer(tube_stations, "tube_stations")
+
+# import the data to table
+arcpy.conversion.ExcelToTable(
+    Input_Excel_File=r"C:\Users\jpmcl\OneDrive\Documents\Economics\Papers (WIP)\Crime and night tubes EXTRA DATA\london_crime_data-2015.xlsx",
+    Output_Table=r"C:\Users\jpmcl\AppData\Local\Temp\ArcGISProTemp14320\Untitled\Default.gdb\london_crime_data2015_ExcelToTable",
+    Sheet="",
+    field_names_row=1,
+    cell_range=""
+)
+
+# geocode them
+arcpy.management.ConvertCoordinateNotation(
+    in_table="london_crime_data2015_ExcelToTable",
+    out_featureclass=r"C:\Users\jpmcl\AppData\Local\Temp\ArcGISProTemp14320\Untitled\Default.gdb\london_crime_d_ConvertCoordi",
+    x_field="Longitude",
+    y_field="Latitude",
+    input_coordinate_format="DDM_2",
+    output_coordinate_format="DDM_2",
+    id_field=None,
+    spatial_reference='PROJCS["British_National_Grid",GEOGCS["GCS_OSGB_1936",DATUM["D_OSGB_1936",SPHEROID["Airy_1830",6377563.396,299.3249646]],PRIMEM["Greenwich",0.0],UNIT["Degree",0.0174532925199433]],PROJECTION["Transverse_Mercator"],PARAMETER["False_Easting",400000.0],PARAMETER["False_Northing",-100000.0],PARAMETER["Central_Meridian",-2.0],PARAMETER["Scale_Factor",0.9996012717],PARAMETER["Latitude_Of_Origin",49.0],UNIT["Meter",1.0]];-5220400 -15524400 10000;-100000 10000;-100000 10000;0.001;0.001;0.001;IsHighPrecision',
+    in_coor_system='GEOGCS["GCS_WGS_1984",DATUM["D_WGS_1984",SPHEROID["WGS_1984",6378137.0,298.257223563]],PRIMEM["Greenwich",0.0],UNIT["Degree",0.0174532925199433]]',
+    exclude_invalid_records="INCLUDE_INVALID"
+)
+
+# for each crime: get the nearest station, and get how far away from it it is
