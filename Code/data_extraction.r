99f429fe379b9f108029bdd792ca071140d74b3a
jacobmcl7 <jacob.mcloughlin@warwick.ac.uk>
2025-10-02 19:32:43 +0100

    small modifications and addition of new files

--- Code/data_extraction.r
+++ Code/data_extraction.r
@@ -6,14 +6,14 @@ library(tidyverse)
 setwd("~/Economics/Papers (WIP)/Crime and night tubes")
 
 # load in the first month of 2015 data
-crime_data <- read.csv("Data/2015-01/2015-01-metropolitan-street.csv", stringsAsFactors = FALSE)
+crime_data <- as.data.frame(read.csv("Data/2015-01/2015-01-metropolitan-street.csv", stringsAsFactors = FALSE))
 
 # merge with the rest of the data
 for (i in 2:12) {
     # get the correct month format
     month <- ifelse(i < 10, paste0("0", i), i)
     # read in the data
-    temp_data <- read.csv(paste0("Data/2015-", month, "/2015-", month, "-metropolitan-street.csv"), stringsAsFactors = FALSE)
+    temp_data <- as.data.frame(read.csv(paste0("Data/2015-", month, "/2015-", month, "-metropolitan-street.csv"), stringsAsFactors = FALSE))
     # merge with the main dataset
     crime_data <- rbind(crime_data, temp_data)
 }  
@@ -22,14 +22,14 @@ for (j in 2016:2017) {
     for (i in 1:12) {
         # get the correct month format
         month <- ifelse(i < 10, paste0("0", i), i)
-        temp_data <- read.csv(paste0("Data/", j, "-", month, "/", j, "-", month, "-metropolitan-street.csv"), stringsAsFactors = FALSE)
+        temp_data <- as.data.frame(read.csv(paste0("Data/", j, "-", month, "/", j, "-", month, "-metropolitan-street.csv"), stringsAsFactors = FALSE))
         crime_data <- rbind(crime_data, temp_data)
     }  
 }
 
 # drop irrelevant columns
 crime_data <- as.data.frame(crime_data) %>%
-    select(-c(Crime.ID, Reported.by, Falls.within, Location, Context))
+    select(-c(Crime.ID, Reported.by, Falls.within, Context))
 
 # drop observations with no coordinates
 crime_data <- crime_data %>%
@@ -38,4 +38,5 @@ crime_data <- crime_data %>%
 # export this as an excel file to be read into ArcGIS
 write.xlsx(crime_data, 'Data/london_crime_data.xlsx')
 
-# note: this exports strangely. We have to open it in excel and re-save it to allow ArcGIS to read it in properly.
+# for some reason this gives a corrupted file: also export it as csv
+write.csv(crime_data, 'Data/london_crime_data.csv')
