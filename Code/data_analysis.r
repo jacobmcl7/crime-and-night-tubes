540b3d40a130efb26c04fa62bb15b3a19327506c
jacobmcl7 <jacob.mcloughlin@warwick.ac.uk>
2025-10-05 23:04:32 +0100

    first basic regression

--- Code/data_analysis.r
+++ Code/data_analysis.r
@@ -17,3 +17,12 @@ simple_did <- lm(num_crimes ~  + factor(location) + factor(period), data = final
 
 # this doesn't work as there are too many factors
 # what can I do about this?
+
+
+# try alternative package
+library(lfe)
+
+simple_did <- felm(num_crimes ~ treatment | location + Month, data = final_data)
+summary(simple_did)
+
+# crimes increase!
